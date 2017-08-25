0091ca6e34ea78f35266b9d91b2b916f58fd7b1b
ValentinLipatov <valentin-ltd@mail.ru>
2017-08-26 00:31:35 +0300

    Update server.js

--- server.js
+++ server.js
@@ -25,15 +25,31 @@ setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
 */
 var express = require('express');
 var app = express();
-var expressWs = require('express-ws')(app);
-var path = require('path');
+app.set('port', (process.env.PORT || 5000));
 
-var server = require('http').createServer(app);
+var server = app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
+
 var io = require('socket.io')(server);
+
+app.use(express.static("./views"));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+app.get('/', function (req, res) {
+    var path = __dirname + '/views/index.html';
+    console.log(path);
+    res.sendFile(path);
+});
 
 io.on('connection', function(socket) {
     socket.on('beep', function(){
-        socket.emit("speed", {data: 5});
+        socket.emit("beep", {data: 5});
         console.log('beep recieved');
     });
 
@@ -45,11 +61,4 @@ io.on('connection', function(socket) {
     socket.on('ios-connection', function(data) {
         console.log('ios connection with message: ' + data);
     });
-
-});
-
-app.set('port', (process.env.PORT || 5000));
-
-app.listen(app.get('port'), function() {
-    console.log('Node app is running on port', app.get('port'));
 });
